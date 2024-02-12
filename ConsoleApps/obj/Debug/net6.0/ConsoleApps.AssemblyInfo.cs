2a15738f9e8133994c76f56714191fc823d0be12
lucasmardby <lucasmardby@gmail.com>
2024-02-12 10:32:18 +0100

    Larger Update

--- ConsoleApps/obj/Debug/net6.0/ConsoleApps.AssemblyInfo.cs
+++ ConsoleApps/obj/Debug/net6.0/ConsoleApps.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a8f55b731a27f7fe61143c5877cb677651e6ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
